66ec0eb302cdee713402284bf369c0b6e30419f1
CaioSouza021 <caio.souza.cs.2003@outlook.com.br>
2024-11-27 15:44:18 -0300

    adicionei o Footer

--- src/Pages/QueroDoar/QueroDoar.jsx
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -1,7 +1,25 @@
+import S from "./QueroDoar.module.scss";
+import livro from "../../assets/livro.png";
+
 export default function QueroDoar() {
   return (
-    <section>
-      <h2>Livres Doados</h2>
+    <section className={S.principal}>
+      <section className={S.container}>
+        <h2>
+          Por favor, preencha o formulário com suas informações e as informações
+          do Livro
+        </h2>
+        <form action="">
+          <div>
+            <img src={livro} alt="" />
+            <h3>Informações do Livro</h3>
+          </div>
+          <input type="text" placeholder="Título" />
+          <input type="text" placeholder="Categoria" />
+          <input type="text" placeholder="Autor" />
+          <input type="text" placeholder="Link da Imagem" />
+        </form>
+      </section>
     </section>
   );
 }
